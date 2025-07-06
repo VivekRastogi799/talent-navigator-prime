da6c64aa63cc028953e242002118f33040bdd36e
VivekRastogi799 <70383255+VivekRastogi799@users.noreply.github.com>
2025-07-07 04:43:46 +0530

    Update vite-env.d.ts
    For Ngrock

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
-/// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    port: 8080, // or 5173, if that's what you're using
+    allowedHosts: ['.ngrok-free.app'], // Allow all ngrok domains
+  },
+})
