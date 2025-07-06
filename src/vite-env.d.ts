0cb46001ceec1cf4cae7205f778f508aced8c2d8
VivekRastogi799 <70383255+VivekRastogi799@users.noreply.github.com>
2025-07-07 04:45:51 +0530

    Update vite-env.d.ts
    Undo Last Commit

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,1 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  server: {
-    port: 8080, // or 5173, if that's what you're using
-    allowedHosts: ['.ngrok-free.app'], // Allow all ngrok domains
-  },
-})
+/// <reference types="vite/client" />
